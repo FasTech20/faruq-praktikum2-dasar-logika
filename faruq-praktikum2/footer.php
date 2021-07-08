0ce420c561a6fa1011368f71ec7733ea30b8d1e7
Faruq Aziz Saputra <73377387+FasTech20@users.noreply.github.com>
2021-07-08 13:56:52 +0700

    Add files via upload

---
+++ faruq-praktikum2/footer.php
@@ -0,0 +1,10 @@
+        <div class="kotak">
+            <footer class="footer text-center">
+                <small class="">Faruq Aziz Saputra - 0110220287</small>
+            </footer>
+        </div>
+    </body>
+    <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/scripts.js"></script>
+</html>
